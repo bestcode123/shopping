2560f29af333d637374a0bf30c8e09fcaf379a92
bestcode123 <burgerboy99@icloud.com>
2021-12-17 13:42:17 +0000

    Auth0 Integration

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+import Auth0LoginButton from './components/auth0/login'
+import Auth0LogoutButton from './components/auth0/logout'
 
 ReactDOM.render(
-  <React.StrictMode>
-
-  </React.StrictMode>,
+  <Auth0Provider
+    domain="burgerapps.eu.auth0.com"
+    clientId="tmXS50pmz3n3DRbF6R145S2N88nsq3qH"
+    redirectUri={window.location.origin}
+  >
+    <Auth0LoginButton />
+    <br></br><br></br>
+    <Auth0LogoutButton />
+  </Auth0Provider>,
   document.getElementById('root')
 );
